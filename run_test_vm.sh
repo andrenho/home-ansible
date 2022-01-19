f744690d3340b9efb6734cd457736f42f1300588
André Wagner <andre.nho@gmail.com>
2022-01-19 16:56:15 -0300

    .

---
+++ run_test_vm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+usage() {
+  echo "Usage: $0 [start|stop|info|login] DIST"
+  exit 1
+}
+
+if [ "$#" -ne 2 ]; then usage; fi
+
+case "$1" in
+  start) cd test-vm/$2 && vagrant up && cd ../.. ;;
+   stop) cd test-vm/$2 && vagrant destroy default && cd ../.. ;;
+   info) cd test-vm/$2 && vagrant global-status && cd ../.. ;;
+  login) cd test-vm/$2 && vagrant ssh && cd ../.. ;;
+      *) usage ;;
+esac

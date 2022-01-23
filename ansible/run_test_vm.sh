e6a3be0e62aebc56c39ce49ab9754b9d291cad6e
André Wagner <andre.nho@gmail.com>
2022-01-23 06:40:58 -0300

    .

--- ansible/run_test_vm.sh
+++ ansible/run_test_vm.sh
@@ -14,13 +14,8 @@ case "$1" in
    stop) cd test-vm/$2 && vagrant destroy default && cd ../.. ;;
    info) cd test-vm/$2 && vagrant global-status && vagrant ssh-config default && cd ../.. ;;
   login) cd test-vm/$2 && vagrant ssh && cd ../.. ;;
-ansible) if [ ! -f "variables.yml" ]; then
-           echo '1. Use `generate_passwd.sh` to generate a hashed password.'
-           echo '2. Copy `variables.yml.in` into `variables.yml` and set your preferred values.'
-           exit 1
-         fi
-         ansible-galaxy install -r requirements.yml && \
-         ansible-playbook -i staging -v site.yml
+ansible) ansible-galaxy install -r requirements.yml && \
+         ansible-playbook -i testing -v site.yml
          ;;
       *) usage ;;
 esac

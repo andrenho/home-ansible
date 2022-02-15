e756f57c2f526bb28c6c4df4bceb56f751a02fe6
André Wagner <andre.nho@gmail.com>
2022-02-14 23:54:59 -0300

    .

--- ansible/roles/admingui-dwm/files/config.def.h
+++ ansible/roles/admingui-dwm/files/config.def.h
@@ -30,8 +30,6 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "google-chrome", NULL,  NULL,       1 << 1,       0,           -1 },
 	{ "Spotify",       NULL,  NULL,       1 << 2,       0,           -1 },
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
